1864a8f8cdd0a7a8cd99503c7f1c33901afa1d9a
colantuomo <pcolantuomo@gmail.com>
2024-01-28 22:21:39 -0300

    feat: adding create payment test file

---
+++ src/domain/use-cases/__tests__/create-new-payment.test.ts
@@ -0,0 +1,39 @@
+import { describe, test, expect, vi } from 'vitest'
+import { mercadoPagoService } from '../../../infraestructure/services/mercado-livre';
+import { createNewPayment } from '../create-new-payment';
+
+vi.mock('@prisma/client', () => ({
+    PrismaClient: vi.fn()
+}));
+
+const SUCCESS_DATA = {
+    id: '2193892',
+    status: 'pending',
+}
+
+function spyMercadoPagoCreatePayment() {
+    vi.spyOn(mercadoPagoService, 'create').mockResolvedValue(SUCCESS_DATA as any);
+}
+
+describe('Create new payment', () => {
+    describe('When a new payment was created with success mercado pago request', () => {
+        test('should call mercado pago with correct body', () => {
+            spyMercadoPagoCreatePayment();
+            createNewPayment({
+                amount: 50, installments: 1, orderId: 'order-id-123"', payerEmail: 'test@gmail.com', paymentMethodId: 'visa'
+            });
+
+            const response = {
+                transaction_amount: 50,
+                description: expect.stringContaining('Gerar pagamento para pedido de valor 50 - pedido: order-id-123'),
+                installments: 1,
+                payment_method_id: 'visa',
+                payer: {
+                    email: 'test@gmail.com'
+                }
+            };
+
+            expect(mercadoPagoService.create).toHaveBeenCalledWith(response);
+        });
+    });
+});

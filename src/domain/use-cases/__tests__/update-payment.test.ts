5e418cbdc0d7aa67e08978ebaa9d6fa9f865ed86
Paulo Martins <18310096+colantuomo@users.noreply.github.com>
2024-01-29 00:00:30 -0300

    Feat/adding update payment tests (#11)

--- src/domain/use-cases/__tests__/update-payment.test.ts
+++ src/domain/use-cases/__tests__/update-payment.test.ts
@@ -1,5 +1,40 @@
 import { describe, test, expect, vi } from 'vitest'
+import { mercadoPagoService } from '../../../infraestructure/services/mercado-livre';
+import { updatePaymentByExternalId } from '../update-payment';
+import { repository } from '../../../infraestructure/repository/payment-repository';
 
 vi.mock('@prisma/client', () => ({
     PrismaClient: vi.fn()
 }));
+
+function spyMercadoPagoReadPayment() {
+    vi.spyOn(mercadoPagoService, 'read').mockResolvedValue({
+        id: '2193892',
+        status: 'pending',
+    });
+}
+
+function spySuccesfullRequest() {
+    vi.spyOn(repository, 'updatePaymentByExternalId').mockResolvedValue({
+        id: 'id-123',
+        createdAt: new Date(),
+        orderId: 'order-id-123',
+        amount: 50,
+        status: "PENDING",
+        externalPaymentId: 'external-payment-id-123',
+        updatedAt: new Date(),
+    } as any);
+}
+
+describe('Update payment', () => {
+    describe('When the mercado pago webhook URL was called', () => {
+        test('should update payment based on mercado pago payment status', async () => {
+            spyMercadoPagoReadPayment();
+            spySuccesfullRequest();
+            await updatePaymentByExternalId({ id: '2193892' });
+
+            expect(mercadoPagoService.read).toHaveBeenCalledWith(2193892);
+            expect(repository.updatePaymentByExternalId).toHaveBeenCalledWith('2193892', 'PENDING');
+        });
+    });
+});

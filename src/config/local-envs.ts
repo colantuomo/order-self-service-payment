40b5b061de475e491f0a36979f2c7a6e5d4def08
colantuomo <pcolantuomo@gmail.com>
2024-03-06 16:20:37 -0300

    chore: removing unused code

--- src/config/local-envs.ts
+++ src/config/local-envs.ts
@@ -1,4 +1,3 @@
 export const AMPQ_HOST = process.env.AMPQ_HOST;
 export const AMQP_USER = process.env.AMQP_USER;
 export const AMQP_PASSWD = process.env.AMQP_PASSWD;
-// export const AMPQ_HOST = '';
